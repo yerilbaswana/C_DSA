c2d6687d4a826a0b4b8a50180b6a689d0b93a66c
yerilbaswana <94475595+yerilbaswana@users.noreply.github.com>
2022-07-16 18:41:29 +0530

    Update extra.cpp

--- extra.cpp
+++ extra.cpp
@@ -8,8 +8,9 @@ struct node
     struct node *next;
 };
 
+// Function declarations
 void print_list(struct node *);
-void add_element_at_end(struct node *, int);
+void add_node_at_end(struct node *, int);
 void reverse_list(struct node *);
 
 int main()
@@ -17,56 +18,57 @@ int main()
     int i = 0, n, input;
     struct node *head, *alpha;
 
-    head = (struct node *)malloc(sizeof(struct node));
-    alpha = (struct node *)malloc(sizeof(struct node));
+    head = (struct node *)malloc(sizeof(struct node));      // Dynamically allocating memory for head pointer
+    alpha = (struct node *)malloc(sizeof(struct node));     // Dynamically allocating memory for alpha pointer
 
-    printf("Enter value of n: ");
+    printf("Enter value of n: ");       // To get input n
     scanf("%d", &n);
 
+    // To add a node to the linked list till i < n
     while(i < n)
     {
-        printf("Enter an element: ");
+        printf("Enter an element: ");       // To get input of node
         scanf("%d", &input);
-        add_element_at_end(head, input);
+        add_node_at_end(head, input);        // To add a node at the end of linked list
         i++;
     }
 
-    print_list(head);
+    print_list(head);       // To print the linked list
 
     printf("\n");
 
-    reverse_list(head);
+    reverse_list(head);         // To reverse a linked list
 
-    print_list(head);
+    print_list(head);       // To print the linked list
 }
 
-void add_element_at_end(struct node *head, int input)
+void add_node_at_end(struct node *head, int input)           // To add a node to end of linked list
 {
     struct node *temp, *result;
 
-    temp = (struct node *)malloc(sizeof(struct node));
+    temp = (struct node *)malloc(sizeof(struct node));      // Dynamically allocating memory for temp pointer
     temp->value = input;
     temp->next = NULL;
     result = head;
 
-    while(result != NULL)
+    while(result != NULL)       // Traversing to the end of linked list
         result = result->next;
 
     result = temp;
 }
 
-void print_list(struct node *head)
+void print_list(struct node *head)      // To print the linked list
 {
     struct node *temp = head;
 
-    while(temp != NULL)
+    while(temp != NULL)         // Traversing from start to end of linked list
     {
         printf("%d", temp->value);
         temp = temp->next;
     }
 }
 
-void reverse_list(struct node *head)
+void reverse_list(struct node *head)        // To reverse the linked list
 {
     int i = 0;
     struct node *temp = NULL, *temp2 = NULL;
