6ce385e9356f2c54941afc8f01d895784a101f0d
yerilbaswana <94475595+yerilbaswana@users.noreply.github.com>
2022-07-28 14:23:34 +0530

    Update polynomial_multiplication.cpp

--- polynomial_multiplication.cpp
+++ polynomial_multiplication.cpp
@@ -9,9 +9,9 @@ struct node
 };
 
 // Function declarations
-void add_term_at_end(struct node *, int, int);
-void multiplication(struct node *, struct node *, struct node *);
-void addition(struct node *);
+struct node *add_term_at_end(struct node *, int, int);
+struct node *multiplication(struct node *, struct node *, struct node *);
+struct node *addition(struct node *);
 void print_list(struct node *);
 
 int main()
@@ -19,34 +19,38 @@ int main()
 	struct node *poly1, *poly2, *result;
 	int choice, exponent, coefficient;
 
-	poly1 = (struct node *)malloc(sizeof(struct node));			// Dynamically allocating memory for poly1.
-	poly2 = (struct node *)malloc(sizeof(struct node));			// Dynamically allocating memory for poly1.
-
+	poly1 = NULL;
+	poly2 = NULL;
+	result = NULL;
+	
 	printf("Enter the polynomial the term belongs to: ");
 	scanf("%d", &choice);
 
-	while(choice != 0)		// Loops till choice != 0. 0 signifies end of input.
+	while(choice)		// Loops till choice != 0. 0 signifies end of input.
 	{
 		printf("Enter the coefficient and the exponent of the term in decreasing order\n");		
 		scanf("%d %d", &coefficient, &exponent);		// Input of polynomial number, coefficient and exponent
 		if(choice == 1)
-			add_term_at_end(poly1, coefficient, exponent);			// Adding at end since linked list to be arranged in decreasing order.
+			poly1 = add_term_at_end(poly1, coefficient, exponent);			// Adding at end since linked list to be arranged in decreasing order.
 		else
-			add_term_at_end(poly2, coefficient, exponent);
+			poly2 = add_term_at_end(poly2, coefficient, exponent);
 
 		printf("Enter the polynomial the term belongs to: ");		
 		scanf("%d", &choice);		// Input of polynomial number, coefficient and exponent.
 	}
+	print_list(poly1);
+	print_list(poly2);
 
-	multiplication(poly1, poly2, result);			// Product of poly1 and poly2. Stored in result.
+	result = multiplication(poly1, poly2, result);			// Product of poly1 and poly2. Stored in result.
 
+	printf("The product of the two polynomials is:\n");
 	print_list(result);
 
 	return 0;
 }
 
 // Function definitions
-void add_term_at_end(struct node *head, int coefficient, int exponent)			// Adding the term at the end
+struct node *add_term_at_end(struct node *poly, int coefficient, int exponent)			// Adding the term at the end
 {
 	struct node *temp;
 	temp = (struct node *)malloc(sizeof(struct node));
@@ -55,46 +59,50 @@ void add_term_at_end(struct node *head, int coefficient, int exponent)			// Addi
 	temp->expnt = exponent;
 	temp->next = NULL;
 
-	if(head == NULL)
-		head = temp;
+	if(poly == NULL)
+		poly = temp;
 	else
 	{
-		while(head->next != NULL)
-			head = head->next;
+		while(poly->next != NULL)
+			poly = poly->next;
 
-		head->next = temp;
+		poly->next = temp;
 	} 
+	return poly;
 }
 
-void multiplication(struct node *poly1, struct node *poly2, struct node *result)		// Product of poly1 and poly2. Stored in result.
+struct node *multiplication(struct node *poly1, struct node *poly2, struct node *result)		// Product of poly1 and poly2. Stored in result.
 {
 	struct node *temp;
 	temp = (struct node *)malloc(sizeof(struct node));
+	temp = poly2;
+	int coefficient, exponent;
 
 	while(poly1 != NULL)
 	{
 		while(poly2 != NULL)
 		{
-			temp->coef = poly1->coef * poly2->coef;
-			temp->expnt = poly1->expnt + poly2->expnt;
-			temp->next = NULL;
-			add_term_at_end(result, temp->coef, temp->expnt);
+			coefficient = poly1->coef * poly2->coef;
+			exponent = poly1->expnt + poly2->expnt;
+			result = add_term_at_end(result, coefficient, exponent);
 			poly2 = poly2->next;
 		}
+		poly2 = temp;
 		poly1 = poly1->next;
 	}
-	addition(result);
+	result = addition(result);
+	return result;
 }
 
-void addition(struct node *result)			// To add the terms with same exponent
+struct node *addition(struct node *result)			// To add the terms with same exponent
 {
-	struct node *temp;
+	struct node *temp, *temp2;
 	temp = (struct node *)malloc(sizeof(struct node));
 	temp = result;
 
-	while(result != NULL)
+	while(result->next != NULL)
 	{
-		temp = result->next;
+		temp = result;
 		while(temp->next != NULL)
 		{
 			if(result->expnt == temp->next->expnt)
@@ -108,15 +116,15 @@ void addition(struct node *result)			// To add the terms with same exponent
 		}
 		result = result->next;
 	}
+	return result;
 }
 
 void print_list(struct node *result)			// To print the list
 {
-	printf("\n");
 	while(result != NULL)
 	{
-		printf("%d\t", result->coef);
-		printf("%d", result->expnt);
+		printf("Coefficient - %d ", result->coef);
+		printf(" Exponent - %d\n", result->expnt);
 		result = result->next;
 	}
 }
